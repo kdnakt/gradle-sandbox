1fbdbea6fe0005aff4d636c55dafd68c77fdbcc3
kdnakt <a.kid.1985@gmail.com>
2021-07-17 19:05:58 +0900

    define default tasks

---
+++ default-tasks/build.gradle.kts
@@ -0,0 +1,20 @@
+defaultTasks("clean", "run")
+
+task("clean") {
+	doLast {
+		println("Default Cleaning!")
+	}
+}
+
+tasks.register("run") {
+	doLast {
+		println("Default Running!")
+	}
+}
+
+tasks.register("other") {
+	doLast {
+		println("I'm not a default task!")
+	}
+}
+
