091df7262c7b9a8857197feed0954a5a387ff4d8
kdnakt <a.kid.1985@gmail.com>
2021-07-19 08:31:19 +0900

    access tasks by type

--- access-tasks/build.gradle.kts
+++ access-tasks/build.gradle.kts
@@ -1,3 +1,4 @@
+// access via tasks collection
 tasks.register("hello")
 tasks.register<Copy>("copy")
 
@@ -7,3 +8,14 @@ println(tasks.named<Copy>("copy").get().destinationDir) // null
 
 // if the task was added by a plugin
 // println(tasks.hello)
+
+// access by type
+tasks.withType<Tar>().configureEach {
+	enabled = false
+}
+
+tasks.register("test") {
+	dependsOn(tasks.withType<Copy>())
+}
+
+
