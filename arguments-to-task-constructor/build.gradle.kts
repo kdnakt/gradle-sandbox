40fb27d63bd4c2feef767406e579ca82abd780fc
kdnakt <a.kid.1985@gmail.com>
2021-07-22 09:19:14 +0900

    add task action

--- arguments-to-task-constructor/build.gradle.kts
+++ arguments-to-task-constructor/build.gradle.kts
@@ -1,7 +1,12 @@
 abstract class CustomTask @Inject constructor(
 	private val message: String,
 	private val number: Int
-): DefaultTask()
+): DefaultTask() {
+	@TaskAction
+	fun doSomething() {
+		println("$message: $number")
+	}
+}
 
 tasks.register<CustomTask>("myTask", "hello", 42)
 
