a4a4cdd5b81681f8b58afe4d2e3a533a444e7cbe
kdnakt <a.kid.1985@gmail.com>
2021-07-26 10:27:56 +0900

    gradle provider

---
+++ dependency-using-gradle-provider/build.gradle.kts
@@ -0,0 +1,29 @@
+val taskX by tasks.registering {
+	doLast {
+		println("taskX")
+	}
+}
+
+taskX {
+	dependsOn(provider {
+		tasks.filter { task -> task.name.startsWith("lib") }
+	})
+}
+
+tasks.register("lib1") {
+	doLast {
+		println("lib1")
+	}
+}
+
+tasks.register("lib2") {
+	doLast {
+		println("lib2")
+	}
+}
+
+tasks.register("notALib") {
+	doLast {
+		println("notALib")
+	}
+}
