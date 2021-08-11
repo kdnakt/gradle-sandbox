cfaa064a9d70baaed44c555e65440f3dcb0db2fd
kdnakt <a.kid.1985@gmail.com>
2021-08-12 08:18:01 +0900

    assemble and build lifecycle task

--- base-plugin/build.gradle.kts
+++ base-plugin/build.gradle.kts
@@ -11,3 +11,23 @@ val taskX by tasks.registering {
 tasks.check {
 	dependsOn(taskX)
 }
+
+val taskY by tasks.registering {
+	doLast {
+		println("taskY")
+	}
+}
+
+tasks.assemble {
+	dependsOn(taskY)
+}
+
+val taskZ by tasks.registering {
+	doLast {
+		println("taskZ")
+	}
+}
+
+tasks.build {
+	dependsOn(taskZ)
+}
