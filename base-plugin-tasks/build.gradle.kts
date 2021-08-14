9674e57708073ae6ffcd155d0640f03e554396e2
kdnakt <a.kid.1985@gmail.com>
2021-08-14 10:03:16 +0900

    print tasks

--- base-plugin-tasks/build.gradle.kts
+++ base-plugin-tasks/build.gradle.kts
@@ -1,38 +1,30 @@
 plugins {
-	java
+	base
 }
 
-tasks.forEach { println(it) }
+tasks.forEach {
+	println(it)
+}
+
 /**
 task ':assemble'
 task ':build'
-task ':buildDependents'
 task ':buildEnvironment'
-task ':buildNeeded'
 task ':check'
-task ':classes'
 task ':clean'
-task ':compileJava'
-task ':compileTestJava'
 task ':components'
 task ':dependencies'
 task ':dependencyInsight'
 task ':dependentComponents'
 task ':help'
 task ':init'
-task ':jar'
 task ':javaToolchains'
-task ':javadoc'
 task ':kotlinDslAccessorsReport'
 task ':model'
 task ':outgoingVariants'
 task ':prepareKotlinBuildScriptModel'
-task ':processResources'
-task ':processTestResources'
 task ':projects'
 task ':properties'
 task ':tasks'
-task ':test'
-task ':testClasses'
 task ':wrapper'
 */
