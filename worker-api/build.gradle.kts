88f487712d1c0f50e077ccd63d6b9942d4ff0705
kdnakt <a.kid.1985@gmail.com>
2021-08-28 16:40:38 +0900

    using worker api

---
+++ worker-api/build.gradle.kts
@@ -0,0 +1,42 @@
+interface ReverseParams: WorkParameters {
+	val fileToReverse: RegularFileProperty
+	val destDir: DirectoryProperty
+}
+
+abstract class ReverseFile @Inject constructor(
+	val fileSystemOperations: FileSystemOperations
+): WorkAction<ReverseParams> {
+	override fun execute() {
+		fileSystemOperations.copy {
+			from(parameters.fileToReverse)
+			into(parameters.destDir)
+			filter { line: String -> line.reversed() }
+		}
+	}
+}
+
+abstract class ReverseFiles @Inject constructor(
+	private val exec: WorkerExecutor
+): SourceTask() {
+	@get: OutputDirectory
+	abstract val outputDir: DirectoryProperty
+
+	@TaskAction
+	fun reverseFiles() {
+		val queue = exec.noIsolation()
+
+		source.forEach { file ->
+			queue.submit(ReverseFile::class) {
+				fileToReverse.set(file)
+				destDir.set(outputDir)
+			}
+		}
+	}
+}
+
+tasks.register<ReverseFiles>("rev") {
+	group = "custom"
+	description = "reverse files"
+	setSource(file("src"))
+	outputDir.set(file("target"))
+}
